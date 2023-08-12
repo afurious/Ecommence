47dd2b670e76554e25c94a33f565c85351d02eb1
afurious <Misraisiguzor@gmail.com>
2023-08-12 11:39:30 +0100

    changed slider to alice-carousel

--- app/component/Slide.jsx
+++ app/component/Slide.jsx
@@ -1,34 +1,36 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Autoplay } from "swiper";
 import Image from "next/image";
-import "swiper/css/autoplay";
 
-import "swiper/css";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+// import "swiper/css/autoplay";
+
+// import "swiper/css";
+const items = [
+  <Image src="/images/slide/1.png" alt="" width={358} height={332} />,
+  <Image src="/images/slide/2.png" alt="" width={358} height={332} />,
+  <Image src="/images/slide/3.png" alt="" width={358} height={332} />,
+  <Image src="/images/slide/4.png" alt="" width={358} height={332} />,
+];
 
 const Slide = ({ delay }) => {
   return (
-    <Swiper
-      slidesPerView={1}
-      modules={[Autoplay]}
-      loop={true}
-      autoplay={{ delay }}
-      // className="mffr-1"
-    >
-      <SwiperSlide>
-        <Image src="/images/slide/1.png" alt="" width={358} height={332} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src="/images/slide/2.png" alt="" width={358} height={428} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src="/images/slide/3.png" alt="" width={358} height={444} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Image src="/images/slide/4.png" alt="" width={358} height={428} />
-      </SwiperSlide>
-    </Swiper>
+    <div className="overflow-hidden">
+      <AliceCarousel
+        items={items}
+        autoPlay={true}
+        disableButtonsControls={true}
+        disableDotsControls={true}
+        infinite={true}
+        animationDuration={2000}
+        autoPlayInterval={delay}
+        animationEasingFunction="ease-out"
+      />
+    </div>
   );
 };
 
