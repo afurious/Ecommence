7f949b2d0811681d1b1b75a5068c7b0756a2c3ac
afurious <Misraisiguzor@gmail.com>
2023-08-12 11:42:07 +0100

    just deleted data function

--- app/marketplace/imagecom/page.jsx
+++ app/marketplace/imagecom/page.jsx
@@ -1,23 +1,23 @@
 import ImageGallery from "@/app/component/ImageGallery";
 import React from "react";
 
-async function getData() {
-  const res = await fetch("http://localhost:8000/api/products");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData() {
+//   const res = await fetch("http://localhost:8000/api/products");
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
+//   // Recommendation: handle errors
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+//  }
 
 const page = () => {
-  const data = getData();
-  console.log(data);
+  // const data = getData();
+  // console.log(data);
 
   return (
     <div>
